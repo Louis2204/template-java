5625e8b72287fefb67082eaa0a2c5202b51c2041
Louis <l.hanquiez22@gmail.com>
2020-12-01 19:10:23 +0100

    2eme version

---
+++ scriptperso.js
@@ -0,0 +1,11 @@
+let bouton = document.getElementById('signin');
+
+bouton.addEventListener('click',()=>
+{
+    alert('Bienvenue')
+});
+
+bouton.addEventListener('2click',()=>
+{
+    alert('Vous êtes déconnecté')
+});
